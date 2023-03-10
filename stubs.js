8d564f0d71598a3fc6933626ff151dc2d22844cf
emorris00 <eric.morris01@gmail.com>
2023-03-10 09:26:04 -0700

    Rename from BN to N-EXT

--- stubs.js
+++ stubs.js
@@ -3,11 +3,11 @@ const mocks = ["config", "runtime"];
 
 module.exports = [
 	...modules.map((path) => ({
-		module: `BN/${path}`,
-		path: `<rootDir>/node_modules/better-suitescript-modules/src/BN/${path}.js`,
+		module: `N-EXT/${path}`,
+		path: `<rootDir>/node_modules/better-suitescript-modules/src/N-EXT/${path}.js`,
 	})),
 	...mocks.map((path) => ({
-		module: `BN/${path}`,
+		module: `N-EXT/${path}`,
 		path: `<rootDir>/node_modules/better-suitescript-modules/src/mocks/${path}.js`,
 	})),
 ];
